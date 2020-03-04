da00403d2bf0dc57b5578268b5dcb4ac59a2a610
Axel Thomsen <axelthomsen@hotmail.com>
2020-03-04 14:34:46 +0100

    STAMP

--- Assets/Stamper.cs
+++ Assets/Stamper.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class Stamper : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float timer = 0;
+    Vector3 stampPos;
+    Vector3 currentPos;
+    Vector3 mousePos;
+    Vector3 stampSize;
+    bool canClick = false;
+    bool dragStamp = false;
+
+    public GameObject stampSprite;
+
+    enum DragStates
+    {
+        Dragged,
+        Free
+    };
+
+    DragStates dragState = DragStates.Free;
+
+
     void Start()
     {
-        
+        stampPos = gameObject.transform.position;
+        stampSize = gameObject.transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = -1;
+
+        switch (dragState)
+        {
+            case DragStates.Free:
+                {
+                    gameObject.transform.localScale = stampSize;
+                    currentPos = gameObject.transform.position;
+                    if (currentPos != stampPos)
+                    {
+                        timer = 0;
+                        timer += Time.deltaTime;
+                        gameObject.transform.position = new Vector3(Mathf.Lerp(currentPos.x, stampPos.x, timer / 0.075f), Mathf.Lerp(currentPos.y, stampPos.y, timer / 0.075f), currentPos.z);
+                    }
+
+                    if (canClick)
+                    {
+                        if (Input.GetMouseButtonDown(0) && dragStamp == false)
+                        {
+                            dragState = DragStates.Dragged;
+                        }
+                    }
+
+
+                    break;
+                }
+
+            case DragStates.Dragged:
+                {
+                    gameObject.transform.position = mousePos;
+                    gameObject.transform.localScale = stampSize * 1.2f;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Vector3 spawnPos = new Vector3(mousePos.x, mousePos.y, -0.6f);
+                        Instantiate(stampSprite, spawnPos, Quaternion.identity);
+                        dragState = DragStates.Free;
+                    }
+
+                    break;
+                }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        canClick = true;
+    }
+
+    private void OnMouseExit()
+    {
+        canClick = false;
     }
 }
