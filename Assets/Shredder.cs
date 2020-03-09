03c499e98bbd598ab0d96a73a5ba8fdee821643a
Emil Dymek <emil.dymek@gmail.com>
2020-03-09 16:05:39 +0100

    Shredder Base 2

--- Assets/Shredder.cs
+++ Assets/Shredder.cs
@@ -6,12 +6,38 @@ public class Shredder : MonoBehaviour
 {
     private Stamper Stampstate;
 
+    GameObject LeftArticle;
+    GameObject RightArticle;
+
+    public bool LeftSelected = false;
+    public bool RightSelected = false;
+
     private void Start()
     {
         Stampstate = GameObject.Find("Stamper").GetComponent<Stamper>();
+
+        LeftArticle = GameObject.FindGameObjectWithTag("Left Text");
+        RightArticle = GameObject.FindGameObjectWithTag("Right Text");
     }
     private void Update()
     {
         
+    }
+
+    public void RightSelect()
+    {
+        if (Stampstate.StampSelected == false)
+        {
+            RightSelected = true;
+            LeftSelected = false;
+        }
+    }
+    public void LeftSelect()
+    {
+        if (Stampstate.StampSelected == false)
+        {
+            LeftSelected = true;
+            RightSelected = false;
+        }
     }
 }
