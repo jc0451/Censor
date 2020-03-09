5d354c696039b0bb43c3fdcbd7e9870d9779a9cd
Emil Dymek <emil.dymek@gmail.com>
2020-03-09 15:53:40 +0100

    Help

--- Assets/Shredder.cs
+++ Assets/Shredder.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Shredder : MonoBehaviour
 {
-    
+    private Stamper Stampstate;
+
+    private void Start()
+    {
+        Stampstate = GameObject.Find("Stamper").GetComponent<Stamper>();
+    }
+    private void Update()
+    {
+        
+    }
 }
