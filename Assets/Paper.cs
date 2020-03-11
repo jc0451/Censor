7750a77bf629076ed1bdb727ccfda6b4b6ff015e
Axel Thomsen <Axel.Thomsen.0446@student.uu.se>
2020-03-11 13:31:53 +0100

    Hitboxes instead of buttons on papers

---
+++ Assets/Paper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper : MonoBehaviour
+{
+    GameObject stamper;
+    public GameObject toPublish;
+
+    void Start()
+    {
+        stamper = GameObject.Find("Stamper");
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    private void OnMouseOver()
+    {
+        if (stamper.GetComponent<Stamper>().StampSelected == true && Input.GetMouseButtonDown(0))
+        {
+            stamper.GetComponent<Stamper>().HasStamped();
+            toPublish.SetActive(true);
+        }
+    }
+}
